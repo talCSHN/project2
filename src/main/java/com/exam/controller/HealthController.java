1a54edced0a548b08eb5fdf82fa255c1269086ef
giuk-kim <gimgiug87@gamil.com>
2024-06-19 09:03:13 +0900

    Login first commit

--- src/main/java/com/exam/controller/HealthController.java
+++ src/main/java/com/exam/controller/HealthController.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.exam.dto.HealthDTO;
+import com.exam.dto.MemberDTO;
 import com.exam.service.HealthService;
 
 @Controller
+@SessionAttributes(names = {"login"})
 public class HealthController {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
@@ -36,11 +39,18 @@ public class HealthController {
 	@GetMapping("/healthRetrieve")
 	public String healthRetrieve(@RequestParam String ponum,ModelMap m) {
 		
-		HealthDTO dto = healthService.HealthRetrieve(ponum);
-		m.addAttribute("healthRetrieve", dto);
-		logger.info("logger:{}",dto);
 		
-		return "healthRetrieve";
+		MemberDTO login=(MemberDTO)m.getAttribute("login");
+		
+		if(login!=null) {
+		
+			HealthDTO dto = healthService.HealthRetrieve(ponum);
+			m.addAttribute("healthRetrieve", dto);
+			logger.info("logger:{}",dto);
+		
+			return "healthRetrieve";
+		}
+		return "redirect:login";
 	}
 	
 	
